f9c7764f96b529c90f00e7a163fae86c997e480c
Saad Rasheed <saarasheed@outlook.com>
2018-11-03 19:37:27 -0500

    actually starting

---
+++ default.js
@@ -0,0 +1,17 @@
+(function () {
+
+      const remote = require('electron').remote;
+
+      function init() {
+        document.getElementById("close-btn").addEventListener("click", function (e) {
+          const window = remote.getCurrentWindow();
+          window.close();
+        });
+      };
+
+      document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+          init();
+        }
+      };
+})();
